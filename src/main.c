79505987be99a2e8325e0992566e5c3399ee082f
nikayacenko <yacenkonika05@mail.ru>
2024-12-23 21:49:31 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -10,73 +10,62 @@
 #include <sys/mount.h>
 #include <stdint-gcc.h>
 #include <sys/stat.h>
-#define BUFFER_SIZE 1024 //буфер для ввода
+ 
+#define BUFFER_SIZE 1024
 
-void handle_SIGHUP(int signal){
-  if (signal == SIGHUP){
-    printf("Configuration reloaded\n");
-    exit(0);
-  }
+//sighup
+void handle_SIGHUP(int signal) {
+    if (signal == SIGHUP) {
+        printf("Configuration reloaded\n");
+        exit(0);
+    }
 }
 
-// 10 определить является ли диск загрузочныv \l nvme0n1
-void is_bootable_device(char* device_name) {
-    // Удаляем лишние пробелы
-    while (*device_name == ' ') {
-        device_name++;
+//10
+void disk_check(char* dname) {
+ 
+    while (*dname == ' ') {
+        dname++;
     }
-
-    // Формируем полный путь к устройству
     char full_path[128];
-    snprintf(full_path, sizeof(full_path), "/dev/%s", device_name);
+    snprintf(full_path, sizeof(full_path), "/dev/%s", dname);//в массив fp размером 128 записываем диск, формируем полный путь
 
-    printf("path: %s\n", full_path);
-
-    // Открываем устройство для чтения
-    FILE* device_file = fopen(full_path, "rb");
-    if (device_file == NULL) {
-        printf("Не удалось открыть диск %s!\n", device_name);
-        //return;
-    }
-
-    // Переходим к 510 байту
-    if (fseek(device_file, 510, SEEK_SET) != 0) {
-        printf("Ошибка при смещении к сигнатуре на диске!\n");
-        fclose(device_file);
+    FILE* disk = fopen(full_path, "rb");//открываем устройство для чтения
+    if (disk == NULL) {
+        printf("error\n");
         return;
     }
-
-    // Считываем последние два байта
+ 
+    if (fseek(disk, 510, SEEK_SET) != 0) {//переходим к 510 байту
+        printf("error\n");
+        fclose(disk);
+        return;
+    }
     uint8_t signature[2];
-    if (fread(signature, 1, 2, device_file) != 2) {
-        printf("Ошибка при чтении сигнатуры диска!\n");
-        fclose(device_file);
+    if (fread(signature, 1, 2, disk) != 2) {//считываем последние 2 байта (1 объект размером 2)
+        printf("error\n");
+        fclose(disk);
         return;
     }
-    fclose(device_file);
-
-    // Проверка сигнатуры на 55 AA
+    fclose(disk);
     if (signature[0] == 0x55 && signature[1] == 0xAA) {
-        printf("Диск %s является загрузочным.\n", device_name);
+        printf("disk %s является загрузочным.\n", dname);
     } else {
-        printf("Диск %s не является загрузочным.\n", device_name);
+        printf("disk %s не является загрузочным.\n", dname);
     }
 }
 
-
-
-
-//12 по 'mem <procid>' получить дамп памяти процесса
-bool copy(char* path1, char* path2) {
+//12
+bool append(char* path1, char* path2) {
     FILE *f1 = fopen(path1, "a");
     FILE *f2 = fopen(path2, "r");
     if (!f1 || !f2) {
         printf("Error while reading file %s\n", path2);
         return false;
     }
-    char buf[1024];
+    char buf[256];
 
-    while (fgets(buf, 1024, f2) != NULL) {
+    while (fgets(buf, 256, f2) != NULL) {
         fputs(buf, f1);
     }
     fclose(f1);
@@ -87,108 +76,123 @@ bool copy(char* path1, char* path2) {
 void makeDump(DIR* dir, char* path) {
     FILE* res = fopen("res.txt", "w+");
     fclose(res);
-    struct dirent* ent;
+    struct dirent* ent;//структура для возврата информации о входах в католог
     char* file_path;
-    while ((ent = readdir(dir)) != NULL) {
-
-        asprintf(&file_path, "%s/%s", path, ent->d_name); // asprintf работает
-        if(!copy("res.txt", file_path)) {
+    while ((ent = readdir(dir)) != NULL) {//чтение следующего входа из католога, если входов нет - ошибка
+        asprintf(&file_path, "%s/%s", path, ent->d_name); //запись в выделенную строку пути и пути к файлу внутри каталога
+        if(!append("res.txt", file_path)) {
             return;
         }
     }
-    printf("Dump completed!\n");
+    printf("succesfull dump\n");
 }
 
+ 
 int main() {
-    
     char input[BUFFER_SIZE];
-    FILE* file_h = fopen("history.txt", "a");              
-    signal(SIGHUP, handle_SIGHUP);
-    do {  
-        bool f = false;  
-        printf("$ ");
+    
+// По сигналу SIGHUP вывести "Configuration reloaded"
+        signal (SIGHUP, handle_SIGHUP);
+        FILE* file_history = fopen("history.txt", "a");
+    do {
+        printf("USER$ ");
         fflush(stdout);
-        // // Чтение ввода с клавиатуры, проверка на EOF (Ctrl+D)
+        bool check=false;
+
+        // input
         if (fgets(input, BUFFER_SIZE, stdin) == NULL) {
             printf("\nЗавершение работы (Ctrl+D)\n");
             break; // выход из цикла при EOF
         }
-
-        //Убираем символ новой строки, если он есть
+ 
+        // \n
         input[strcspn(input, "\n")] = '\0';
-
-        // Проверяем на команды выхода
+ 
+        // exit
         if (strcmp(input, "exit") == 0 || strcmp(input, "\\q") == 0) {
             printf("Завершение работы (exit/\\q)\n");
             break;
         }
-        
-        // Сохраняем введённую команду в историю (если есть место)
-        if(file_h) {
-            fprintf(file_h, "%s\n", input);
-          fclose(file_h);
-          file_h = fopen("history.txt", "a");
+
+        // history
+        if(file_history) {
+            fprintf(file_history, "%s\n", input);
+          fclose(file_history);
+          file_history = fopen("history.txt", "a");
         }
-        if (strcmp(input, "echo $PATH") == 0){
+
+        //echo $PATH
+        if(strcmp(input, "e $PATH")==0) {
+            check=true;
             char *path = getenv("PATH");
-            if (path != NULL){
+            if (path!=NULL){
                 printf("%s\n", path);
-            } else{
-                printf("Переменная PATH не найдена\n");
             }
-            f = true;
+            else {
+                 printf("error \n");
+            }
             continue;
         }
-        //9. run /bin/git
-        if (strncmp(input, "run ", 4) == 0){
-            pid_t p = fork();
-            if (p == 0){
-              char *argv[] = { "sh", "-c", input + 4, 0 };
-              execvp(argv[0], argv);
-              fprintf(stderr, "Failed to exec shell on %s\n", input + 4);
-            
-              f = true;
-              exit(1);
-            }
-            sleep(1);
-            
-        }
-        // 10 \l nvme0n1
-        if (strncmp(input, "\\l", 2) == 0) {
-            char* device_name = input + 3;
-            is_bootable_device(device_name);
-            f = true;
-            continue;
-        }
-        // Проверяем команду echo
+ 
+        // echo
         if (strncmp(input, "echo ", 5) == 0) {
-            printf("%s\n", input + 5); // Выводим всё, что после "echo "
-            f = true;
+            check=true;
+            printf("%s\n", input + 5);
             continue;
         }
-        //12 htop+  \l pid
+
+       //binary
+
+        if (strncmp(input, "run ", 4) == 0){
+            check=true;
+            pid_t p = fork();//pid_t тип данных для id процесса, fork создает новый процесс
+            if (p == 0){
+              char *argv[] = { "sh", "-c", input + 4, 0 };//sh двоичный исп файл, который мы запускаем
+              execvp(argv[0], argv);
+              fprintf(stderr, "Failed to exec shell on %s", input + 4);
+              
+              exit(1);
+             
+            }
+sleep(1);
+continue;
+}
+
+
+  
+        
+       //10. По `\l /dev/sda` определить является ли диск загрузочным
+         if (strncmp(input, "\\l", 2) == 0) {
+            check=true;
+            char* dname = input + 3;
+            disk_check(dname);
+            continue;
+        }
+        
+       //12
         if (strncmp(input, "\\mem ", 5) == 0) {
             char* path;
-            asprintf(&path, "/proc/%s/map_files", input+5);
-            DIR* dir = opendir(path);
+            asprintf(&path, "/proc/%s/map_files", input+5);//вывод данных в выделенную в памяти строку
+
+            DIR* dir = opendir(path);//открытие потока католога
             if (dir) {
                 makeDump(dir, path);
             }
             else {
                 printf("Process not found\n");
             }
-            f = true;
+            check = true;
             continue;
         }
-        
-        if (f == false){
-          printf("There is no such command!\n");
-          
+
+        if(check==false){
+        printf("there is no command: %s\n", input);
         }
+       
     }
     while (!feof(stdin));
-
-
+ 
+    
+ 
     return 0;
 }
-
